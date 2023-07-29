86396e31f35973bc6186cb64616cf8afbb50076c
MD ARIFUZZAMAN <125180816+arifuzzamanshakil@users.noreply.github.com>
2023-07-29 20:08:35 +0600

    nav

--- src/components/layouts/Navbar.jsx
+++ src/components/layouts/Navbar.jsx
@@ -56,30 +56,12 @@ export const Navbar = (props) => {
 
   return (
     <div className='px-0 fixed z-30 w-screen'>
+        
       <div className='bg-[#130524] text-center'>
         <p className='text-sm h-[34px] py-1 md:text-lg backdrop-blur-[13.5px] text-white tracking-[1.4px] md:tracking-[1.8px]'>{data && data.topnav[`${trans}`]}<strong> <a href="tel:911" className=' text-white hover:text-[#FF5AC0] '>{data && data.topnavCall[`${trans}`]}</a></strong></p>
       </div>
-      <div className="items-center px-4 md:px-0 py-0 bgColor">
-        <div className='flex items-center justify-between mx-auto xl:w-[1160px] lg:w-[1000px] sm:w-full lg:px-0 sm:px-8 xl:px-0 w-[340px]'>
-          <div className='languageSec'>
-            <div className="w-[33px] md:w-[95px] text-xl md:text-2xl font-normal text-[#FFC1E5] hover:text-white hover:ease-in duration-75 border-none overflow-hidden">
-              <button onClick={toggleLanguage} className='langShadow'> {/*Use toggleLanguage function here  */}
-                {lang === "en" ? "Español" : "English"}
-              </button>
-            </div>
-          </div>
-
-          <a href='/' className='cursor-pointer w-[160px] md:w-auto md:h-auto'>
-            <img src={data && data.logoImg[`${trans}`]} alt="Logo" />
-          </a>
-
-          <button className="w-[35px] md:w-[95px] flex justify-end" onClick={showNavbar} >
-              <HamBug /> {/* humbugger button  */}
-          </button>
-        </div>
-
-        {/* dropdown menu  */}
-        <div className="navHeader p-0">
+      {/* dropdown menu  */}
+      <div className="navHeader z-20 p-0">
             <nav ref={navRef} className="flex flex-col left-0 fixed -top-full h-full w-full items-center justify-center duration-[1s] gap-12 bg-no-repeat bg-right bg-cover md:bg-center">
 
               <div className="absolute top-5 md:top-14 px-4 flex items-center justify-between mx-auto xl:w-[1160px] lg:w-[1000px] sm:w-full lg:px-0 sm:px-8 xl:px-0 w-[340px]">
@@ -112,6 +94,26 @@ export const Navbar = (props) => {
               <button className="rescueBtn mt-12 border border-[#E5AEEE] hover:border-[#97E4FC] border-solid rounded-[10px] py-[14px] px-10 font-Rajdhani font-bold text-[20px] text-[#FFC1E5] hover:text-[#97E4FC]  absolute bottom-10 md:bottom-0 md:relative mx-auto">{data && data.navBtn[`${trans}`]} </button>
             </nav>
           </div>
+      <div className="items-center px-4 md:px-0 py-0 bgColor">
+        <div className='flex items-center justify-between mx-auto xl:w-[1160px] lg:w-[1000px] sm:w-full lg:px-0 sm:px-8 xl:px-0 w-[340px]'>
+          <div className='languageSec'>
+            <div className="w-[33px] md:w-[95px] text-xl md:text-2xl font-normal text-[#FFC1E5] hover:text-white hover:ease-in duration-75 border-none overflow-hidden">
+              <button onClick={toggleLanguage} className='langShadow'> {/*Use toggleLanguage function here  */}
+                {lang === "en" ? "Español" : "English"}
+              </button>
+            </div>
+          </div>
+
+          <a href='/' className='cursor-pointer w-[160px] md:w-auto md:h-auto'>
+            <img src={data && data.logoImg[`${trans}`]} alt="Logo" />
+          </a>
+
+          <button className="w-[35px] md:w-[95px] flex justify-end" onClick={showNavbar} >
+              <HamBug /> {/* humbugger button  */}
+          </button>
+        </div>
+
+      
       </div>
     </div>
   );
